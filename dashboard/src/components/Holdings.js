75b3d12bebb8a8e230266d043cd4610b6759be05
Mohd Kashan <kashan0716@gmail.com>
2025-08-12 21:33:24 +0530

    Update Holdings.js

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3003/allHoldings").then((res) => {
+    axios.get("https://tradingapp-backend-517h.onrender.com").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
